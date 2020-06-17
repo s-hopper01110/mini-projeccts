00ff23c75c63d1137cadf984455a49cec9ea356e
Sheila <s.hopper01110@gmail.com>
2020-06-16 22:08:29 -0400

    increment functionality working / color also changes when in positive range

--- index.js
+++ index.js
@@ -28,13 +28,21 @@ function quoteRandomizer() {
 let a = 0; //note: global variable
 document.getElementById('numericalDisplay').innerHTML = a;
 
+
 function decrementButton() {
 	let b = (a -= 1);
 	document.getElementById('numericalDisplay').innerHTML = b;
-
+    
 	if (b < 0) {
 		document.getElementById('numericalDisplay').style.color = '#d5f7f3';
 	}
 }
 
-function incrementButton() {}
+function incrementButton() {
+    let c = (a += 1);
+    document.getElementById('numericalDisplay').innerHTML = c;
+    
+    if (c > 0) {
+        document.getElementById('numericalDisplay').style.color = '#dec1f6'
+    }
+}
