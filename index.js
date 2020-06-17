873215cbc08131b092df267821a24a14b0002612
Sheila <s.hopper01110@gmail.com>
2020-06-16 21:58:20 -0400

    decrement functionality working / color also changes upon decrement

--- index.js
+++ index.js
@@ -1,34 +1,40 @@
-
 //Background color change project
 function colorRandomizer() {
-    let randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);
-    return randomColor;
+	let randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+	return randomColor;
 }
 
 function randomizerButton() {
-    document.getElementById("colorChange").style.backgroundColor = colorRandomizer() ;
-
+	document.getElementById('colorChange').style.backgroundColor = colorRandomizer();
 }
 
 //Quote Generator Project:
-let quotes = ["Love For All, Hatred For None. – Khalifatul Masih III", "Every moment is a fresh beginning. – T.S Eliot", "Simplicity is the ultimate sophistication. – Leonardo da Vinci", "All limitations are self-imposed. – Oliver Wendell Holmes", "All limitations are self-imposed. – Oliver Wendell Holmes", "Yesterday you said tomorrow. Just do it. – Nike"]
+let quotes = [
+	'Love For All, Hatred For None. – Khalifatul Masih III',
+	'Every moment is a fresh beginning. – T.S Eliot',
+	'Simplicity is the ultimate sophistication. – Leonardo da Vinci',
+	'All limitations are self-imposed. – Oliver Wendell Holmes',
+	'All limitations are self-imposed. – Oliver Wendell Holmes',
+	'Yesterday you said tomorrow. Just do it. – Nike'
+];
 
 function quoteRandomizer() {
-    let randomQuote = Math.floor(Math.random()*quotes.length)
-    document.getElementById("quoteInput").innerHTML = quotes[randomQuote]
+	let randomQuote = Math.floor(Math.random() * quotes.length);
+	document.getElementById('quoteInput').innerHTML = quotes[randomQuote];
 }
 
- //Increment/Decrement Counter Project:
+//Increment/Decrement Counter Project:
+//Decrement:
+let a = 0; //note: global variable
+document.getElementById('numericalDisplay').innerHTML = a;
 
-//note: globlal variable
-let a = 0;
-document.getElementById("numericalDisplay").innerHTML = a;
+function decrementButton() {
+	let b = (a -= 1);
+	document.getElementById('numericalDisplay').innerHTML = b;
 
-function decrementButton(){
-let b = a -= 1;
-document.getElementById("numericalDisplay").innerHTML = b
+	if (b < 0) {
+		document.getElementById('numericalDisplay').style.color = '#d5f7f3';
+	}
 }
 
-function incrementButton(){
-
-}
+function incrementButton() {}
